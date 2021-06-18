8df3039f6db8b6308ccd57ebd68889cd5b80e978
Pranjali1999 <pranjaliajmire99@gmail.com>
2021-06-18 09:26:59 +0600

    New File : JavaScript assignment1

---
+++ Pranjali/JavaScript/PRACT/June14/demo3.js
@@ -0,0 +1,6 @@
+debugger;
+document.write("Good Morning!!");
+//window.alert("I am inside alert");
+console.log("I am inside console");
+document.getElementById("pid").innerHTML = "<h3>Inner Text</h3>";
+document.getElementById("pid1").innerHTML = "<h3>Inner Text</h3>";
