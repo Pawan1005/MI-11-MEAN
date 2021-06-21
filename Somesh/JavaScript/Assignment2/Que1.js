a2908d2b4ca46fe044660b3f78cb49c9f33266f8
somesh6797 <smshrndv@gmail.com>
2021-06-21 09:45:29 +0530

    Adding Assignment2

---
+++ Somesh/JavaScript/Assignment2/Que1.js
@@ -0,0 +1,24 @@
+const inputs = document.getElementsByTagName("input");
+let numbers = [];
+Array.from(inputs).forEach((input) => {
+    input.addEventListener("change", (e) => {
+        numbers.push(parseInt(e.target.value));
+    });
+});
+let secondLarge = () => {
+    console.log(numbers);
+    let firstMax = 0;
+    let secondMax = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (firstMax < numbers[i]) {
+            secondmax = firstMax;
+            firstMax = numbers[i];
+        }
+        if (numbers[i] != firstMax && numbers[i] > secondMax) {
+            secondMax = numbers[i];
+        }
+    }
+    document.getElementsByClassName(
+        "output"
+    )[0].innerText = `The second largest element is ${secondMax}`;
+};
