24660d2e0f5c8ab801c532200713dd1a6ef75767
Ashwin2812 <ashwin.shivhare8@gmail.com>
2021-07-20 14:09:51 +0530

    Modified Assignment

--- Ashwin/JavaScript/15_Assignment/15_Assignment_1.js
+++ Ashwin/JavaScript/15_Assignment/15_Assignment_1.js
@@ -1,21 +1,17 @@
- 
-class BankDet {
+ class BankDet {
     constructor() {
         this.acc_no = 0;
         this.acc__name = "";
         this.bal = 0;
     }
-
     getData(acc_no, acc__name, bal) {
         this.acc_no = acc_no;
         this.acc__name = acc__name;
         this.bal = bal;
     }
-
     deposit(amountAdd) {
         this.bal += amountAdd;
     }
-
     withdraw(amount) {
         try {
             if (amount > this.bal) {
@@ -34,46 +30,34 @@ class BankDet {
         }
     }
 }
-
 var bankPerson = null;
 var counter = 0;
-
 function getBankData() {
-    // Set Empty For New Person data
     document.getElementById('warningMessage').innerHTML = "";
     document.getElementById('errorMessage').innerHTML = "";
-
     let accountNumber = parseInt(prompt('Enter a Account Number:'));
     let accountName = prompt('Enter a Account Holder Name:');
     let balance = parseInt(prompt('Enter a Total Balance:'));
-
     bankPerson = new BankDet();
     bankPerson.getData(accountNumber, accountName, balance);
-
     counter++;
 }
-
 function depositAmount() {
     let amountDeposit = parseInt(prompt('Enter a Amount to Be Deposit:'));
     bankPerson.deposit(amountDeposit);
 }
-
 function withdrawAmount() {
     let amountWithdraw = parseInt(prompt('Enter a Amount to Be Withdraw:'));
     bankPerson.withdraw(amountWithdraw);
 }
-
 function displayDetails() {
     let table = document.getElementById('table');
-
     table.style.display = "inline-block";
-
     let row = table.insertRow();
     let rowData1 = row.insertCell(0);
     let rowData2 = row.insertCell(1);
     let rowData3 = row.insertCell(2);
     let rowData4 = row.insertCell(3);
-
     rowData1.innerHTML = `${counter}`;
     rowData2.innerHTML = `${bankPerson.acc_no}`;
     rowData3.innerHTML = `${bankPerson.acc__name}`;
--- Ashwin/JavaScript/15_Assignment/15_Assignment_2.js
+++ Ashwin/JavaScript/15_Assignment/15_Assignment_2.js
@@ -1,70 +1,71 @@
 class SalesPerson {
-    constructor() {
-        this.person__id = 0;
-        this.person__name = "";
-    }
-    getData(person__id, person__name) {
-        this.person__id = person__id;
-        this.person__name = person__name;
-    }
+  constructor () {
+    this.person__id = 0
+    this.person__name = ''
+  }
+  getData (person__id, person__name) {
+    this.person__id = person__id
+    this.person__name = person__name
+  }
 }
 class TotalSales extends SalesPerson {
-    constructor() {
-        super();
-        this.total__sales = 0;
+  constructor () {
+    super()
+    this.total__sales = 0
+  }
+  calculateSales (amount) {
+    try {
+      if (amount < 0 || !amount) {
+        throw new Error('Please Enter Proper sales')
+      } else if (amount >= 0 && amount <= 5000) {
+        this.total__sales = amount - amount * 0.02
+        document.getElementById('errorMessage').innerHTML = ''
+      } else if (amount >= 5001 && amount <= 12000) {
+        this.total__sales = amount - amount * 0.05
+        document.getElementById('errorMessage').innerHTML = ''
+      } else if (amount >= 12001 && amount <= 50000) {
+        this.total__sales = amount - amount * 0.1
+        document.getElementById('errorMessage').innerHTML = ''
+      } else {
+        this.total__sales = amount - amount * 0.15
+        document.getElementById('errorMessage').innerHTML = ''
+      }
+    } catch (error) {
+      document.getElementById(
+        'errorMessage'
+      ).innerHTML = `<b style="color: red">${error.message}</b>`
     }
-    calculateSales(amount) {
-        try {
-            if (amount < 0 || (!amount)) {
-                throw new Error("Please Enter Proper sales");
-            }
-            else if (amount >=0 && amount <=5000) {
-                this.total__sales = amount - (amount * 0.02);
-                document.getElementById('errorMessage').innerHTML = '';
-            }
-            else if (amount >=5001 && amount <=12000) {
-                this.total__sales = amount - (amount * 0.05);
-                document.getElementById('errorMessage').innerHTML = '';
-            }
-            else if (amount >=12001 && amount <= 50000) {
-                this.total__sales = amount - (amount * 0.1);
-                document.getElementById('errorMessage').innerHTML = '';
-            }
-            else {
-                this.total__sales = amount - (amount * 0.15);
-                document.getElementById('errorMessage').innerHTML = '';
-            }
-        }
-        catch (error) {
-            document.getElementById('errorMessage').innerHTML = `<b style="color: red">${error.message}</b>`;
-        }
-    }
+  }
 }
-var salesPerson = null;
-var counter = 0;
-var checkCall = false;
-function getSalesData() {
-    checkCall = false;
-    let salesId = parseInt(prompt('Enter a Sales Id:'));
-    let personName = prompt('Enter a Sales Person Name:');
-    salesPerson = new TotalSales();
-    salesPerson.getData(salesId, personName);
-    counter++;
+var salesPerson = null
+var counter = 0
+var checkCall = false
+function getSalesData () {
+  checkCall = false
+  let salesId = parseInt(prompt('Enter a Sales Id:'))
+  let personName = prompt('Enter a Sales Person Name:')
+  salesPerson = new TotalSales()
+  salesPerson.getData(salesId, personName)
+  counter++
 }
-function inputSales() {
-    checkCall = true;
-    let salesAmount = parseInt(prompt('Enter a Sales Amount:'));
-    salesPerson.calculateSales(salesAmount);
+function inputSales () {
+  checkCall = true
+  let salesAmount = parseInt(prompt('Enter a Sales Amount:'))
+  salesPerson.calculateSales(salesAmount)
 }
-function displayDetails() {
-    let table = document.getElementById('table');
-    table.style.display = "inline-block";
-    let row = table.insertRow();
-    let rowData1 = row.insertCell(0);
-    let rowData2 = row.insertCell(1);
-    let rowData3 = row.insertCell(2);
-    rowData1.innerHTML = `${counter}`;
-    rowData2.innerHTML = `${salesPerson.person__id}`;
-    rowData3.innerHTML = `${salesPerson.person__name}`;
-    checkCall ? document.getElementById("totalSales").innerHTML += `<br>Total Sales With Commision: <b>${salesPerson.total__sales}</b>` : '';
+function displayDetails () {
+  let table = document.getElementById('table')
+  table.style.display = 'inline-block'
+  let row = table.insertRow()
+  let rowData1 = row.insertCell(0)
+  let rowData2 = row.insertCell(1)
+  let rowData3 = row.insertCell(2)
+  rowData1.innerHTML = `${counter}`
+  rowData2.innerHTML = `${salesPerson.person__id}`
+  rowData3.innerHTML = `${salesPerson.person__name}`
+  checkCall
+    ? (document.getElementById(
+        'totalSales'
+      ).innerHTML += `<br>Total Sales With Commision: <b>${salesPerson.total__sales}</b>`)
+    : ''
 }
